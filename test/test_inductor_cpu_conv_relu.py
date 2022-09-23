08018a556667848ec0d8667a640a4093d202db65
chunyuan <chunyuan.wu@intel.com>
2022-09-23 13:38:06 +0000

    check bias (true, false) and shape (2d, 3d) in UT

--- test/test_inductor_cpu_conv_relu.py
+++ test/test_inductor_cpu_conv_relu.py
@@ -4,9 +4,9 @@ from torchdynamo.testing import same
 
 torchdynamo.config.raise_on_backend_error = False
 
-def test_conv_relu():
+def test_conv_relu(bias, input_shape):
     mod = torch.nn.Sequential(
-        torch.nn.Linear(10, 10),
+        torch.nn.Linear(input_shape[-1], 10, bias=bias),
         torch.nn.ReLU(),
     ).eval()            
 
@@ -14,10 +14,27 @@ def test_conv_relu():
     def fn(x):
         return mod(x)
 
-    v = torch.randn(2, 10)
+    v = torch.randn(input_shape)
+
+    # v = v.to(torch.bfloat16)
+    # mod = mod.to(torch.bfloat16)
+
     print("#" * 50)
     result = fn(v)
     print("#" * 50)
     assert same(result, mod(v))
 
-test_conv_relu()
+    print("run")
+    fn(v)
+    fn(v)
+
+
+for input_shape in [
+    [2, 3, 10],
+    [2, 10]]:
+    for bias in [
+        True,
+        False]:
+        test_conv_relu(bias, input_shape)
+
+print("done")
