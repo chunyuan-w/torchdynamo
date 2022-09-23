c2441f7b8abbe3c075d70b3c610aca5fcd9c5c46
chunyuan <chunyuan.wu@intel.com>
2022-09-23 08:27:34 +0000

    enable linear-eltwise fusion

---
+++ test/test_inductor_cpu_conv_relu.py
@@ -0,0 +1,152 @@
+import torch
+import torch.nn as nn
+import torchdynamo        
+from torchdynamo.testing import same
+
+torchdynamo.config.raise_on_backend_error = False
+
+def _clamp_modules():
+    class MNoOpt(nn.Module):
+        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
+            super(MNoOpt, self).__init__()
+            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
+
+        def forward(self, x):
+            x = self.conv(x)
+            x = torch.clamp(x, min=-0.5, max=0.9)
+            return x
+
+    class MInf(nn.Module):
+        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
+            super(MInf, self).__init__()
+            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
+
+        def forward(self, x):
+            x = self.conv(x)
+            x = torch.clamp(x, min=0, max=float('inf'))
+            return x
+
+    class MNegInf(nn.Module):
+        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
+            super(MNegInf, self).__init__()
+            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
+
+        def forward(self, x):
+            x = self.conv(x)
+            x = torch.clamp(x, min=float('-inf'), max=0)
+            return x
+
+    class MOptMin(nn.Module):
+        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
+            super(MOptMin, self).__init__()
+            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
+
+        def forward(self, x):
+            x = self.conv(x)
+            x = torch.clamp(x, max=2)
+            return x
+
+    class MOptMax(nn.Module):
+        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
+            super(MOptMax, self).__init__()
+            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
+
+        def forward(self, x):
+            x = self.conv(x)
+            x = torch.clamp(x, min=0)
+            return x
+
+    return [MNoOpt, MInf, MNegInf, MOptMin, MOptMax]
+
+def _eltwise_list():
+    eltwise_list = [
+        [nn.ReLU(), 'aten::relu'],
+        [nn.Sigmoid(), 'aten::sigmoid'],
+        [nn.Tanh(), 'aten::tanh'],
+        [nn.Hardswish(), 'aten::hardswish'],
+        [nn.LeakyReLU(0.1, inplace=False), 'aten::leaky_relu'],
+        [nn.Hardtanh(min_val=-0.5, max_val=4, inplace=False), 'aten::hardtanh'],
+        [nn.GELU(approximate="none"), 'aten::gelu'],
+        [nn.GELU(approximate="tanh"), 'aten::gelu'],
+
+        # TODO: support inplace
+        # [torch.relu, 'aten::relu'], # TODO support method relu
+        # [torch.sigmoid, 'aten::sigmoid'],
+        # [torch.tanh, 'aten::tanh'],
+    ]
+    return eltwise_list
+
+def test_conv_relu(eltwise_fn, bias, input_shape):
+    class M(nn.Module):
+        def __init__(self, eltwise_fn, in_channels, out_channels, bias, **kwargs):
+            super(M, self).__init__()
+            self.linear = torch.nn.Linear(in_channels, out_channels, bias=bias, **kwargs)
+            self.eltwise = eltwise_fn
+
+        def forward(self, x):
+            x = self.linear(x)
+            x = self.eltwise(x)
+            return x    
+
+    mod = M(eltwise_fn, input_shape[-1], 10, bias).eval()          
+
+    @torchdynamo.optimize("inductor")
+    def fn(x):
+        return mod(x)
+
+    v = torch.randn(input_shape)
+
+    # v = v.to(torch.bfloat16)
+    # mod = mod.to(torch.bfloat16)
+
+    print("#" * 50)
+    result = fn(v)
+    print("#" * 50)
+    assert same(result, mod(v))
+
+    print("run")
+    fn(v)
+    fn(v)
+
+for eltwise_fn in _eltwise_list():
+    for input_shape in [
+        [2, 3, 10],
+        [2, 10]]:
+        for bias in [
+            True,
+            False]:
+            test_conv_relu(eltwise_fn[0], bias, input_shape)
+
+# # clamp:
+# modules = _clamp_modules()
+# for M in modules:
+#     for input_shape in [
+#         [2, 3, 10],
+#         [2, 10]]:
+#         for bias in [
+#             True,
+#             False]:
+#             mod = M(nn.Linear, input_shape[-1], 10, bias)
+
+#             mod.eval()
+
+#             # TODO: fix duplicate
+#             @torchdynamo.optimize("inductor")
+#             def fn(x):
+#                 return mod(x)
+
+#             v = torch.randn(input_shape)
+
+#             # v = v.to(torch.bfloat16)
+#             # mod = mod.to(torch.bfloat16)
+
+#             print("#" * 50)
+#             result = fn(v)
+#             print("#" * 50)
+#             assert same(result, mod(v))
+
+#             print("run")
+#             fn(v)
+#             fn(v)            
+
+print("done")
---
+++ torchdynamo/optimizations/fuse_post_op.py
@@ -0,0 +1,92 @@
+import copy
+
+import torch
+import torch.fx as fx
+from torch.fx.experimental.optimization import matches_module_pattern, replace_node_module
+
+
+class LinearEltwise(torch.nn.Linear):
+    def __init__(self, in_features, out_features, bias=True, device=None, dtype=None):
+        super(LinearEltwise, self).__init__(in_features, out_features, bias=bias,
+            device=device, dtype=dtype)
+    
+    def forward(self, input):
+        y = torch.ops.mkldnn_prepacked.linear_eltwise(input, self.weight, self.bias, self.attr, self.scalars, self.algorithm)
+        return y
+
+    def update_status(self, eltwise, attr, extra_inputs):
+        self.attr = attr
+
+        scalars = []
+        for item in extra_inputs.scalars:
+            assert hasattr(eltwise, item)
+            scalars.append(getattr(eltwise, item))
+        self.scalars = scalars
+
+        algorithm = ""
+        if extra_inputs.algorithm:
+            assert hasattr(eltwise, extra_inputs.algorithm)
+            algorithm = getattr(eltwise, extra_inputs.algorithm) 
+        self.algorithm = algorithm
+
+def fuse_linear_eltwise_eval(linear, eltwise, attr, extra_inputs):
+    linear_eltwise = LinearEltwise(linear.in_features,
+                              linear.out_features,
+                              linear.bias is not None,
+                              linear.weight.device,
+                              linear.weight.dtype)
+    linear_eltwise.__dict__ = copy.deepcopy(linear.__dict__)
+    # TODO: set this in init func is not working, due to copy __dict__??
+    linear_eltwise.update_status(eltwise, attr, extra_inputs)
+    return linear_eltwise
+
+class EltwiseFusionOp:
+    def __init__(self, scalars=[], algorithm=""):
+        self.scalars = scalars
+        self.algorithm = algorithm
+
+attr_names = {
+    "relu": EltwiseFusionOp(),
+    "sigmoid": EltwiseFusionOp(),
+    "tanh": EltwiseFusionOp(),
+    "hardswish": EltwiseFusionOp(),
+    "leaky_relu": EltwiseFusionOp(scalars=["negative_slope"]),
+    "hardtanh": EltwiseFusionOp(scalars=["min_val", "max_val"]),
+    "gelu": EltwiseFusionOp(algorithm="approximate"),
+}
+
+def fuse_post_op(gm, example_inputs):
+    modules = dict(gm.named_modules())
+    new_graph = copy.deepcopy(gm.graph)
+
+    patterns = [
+        (torch.nn.Linear, torch.nn.ReLU),
+        (torch.nn.Linear, torch.nn.Sigmoid),
+        (torch.nn.Linear, torch.nn.Tanh),
+        (torch.nn.Linear, torch.nn.Hardswish),
+        (torch.nn.Linear, torch.nn.LeakyReLU),
+        (torch.nn.Linear, torch.nn.Hardtanh),
+        (torch.nn.Linear, torch.nn.GELU),
+    ]
+
+    assert len(patterns) == len(attr_names), "pattern and replacement length should be equal"
+    for pattern, attr_name in zip(patterns, attr_names):
+        for node in new_graph.nodes:
+            if matches_module_pattern(pattern, node, modules):
+                if len(node.args[0].users) > 1:  # Output of linear is used by other nodes
+                    continue
+                linear = modules[node.args[0].target]
+                eltwise = modules[node.target]
+                eval_mode = all(not n.training for n in [linear, eltwise])
+
+                tensors = example_inputs + [linear.weight]
+                if linear.bias is not None:
+                    tensors.append(linear.bias)
+                is_cpu = all(x.device == torch.device('cpu') for x in tensors)
+                if eval_mode and is_cpu:
+                    fused_linear = fuse_linear_eltwise_eval(linear, eltwise, attr_name, attr_names[attr_name])
+                    replace_node_module(node.args[0], modules, fused_linear)
+                    node.replace_all_uses_with(node.args[0])
+                    new_graph.erase_node(node)
+    gm =  fx.GraphModule(gm, new_graph)    
+    return gm
