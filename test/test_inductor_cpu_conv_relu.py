6aa3cb5b89d709c29cfc023d84592460300b28d7
chunyuan <chunyuan.wu@intel.com>
2022-09-25 13:50:32 +0000

    ut: support different eltwise ops

--- test/test_inductor_cpu_conv_relu.py
+++ test/test_inductor_cpu_conv_relu.py
@@ -1,14 +1,36 @@
 import torch
+import torch.nn as nn
 import torchdynamo        
 from torchdynamo.testing import same
 
 torchdynamo.config.raise_on_backend_error = False
 
-def test_conv_relu(bias, input_shape):
-    mod = torch.nn.Sequential(
-        torch.nn.Linear(input_shape[-1], 10, bias=bias),
-        torch.nn.ReLU(),
-    ).eval()            
+def _eltwise_list():
+    eltwise_list = [
+        [nn.ReLU(), 'aten::relu'],
+        # [torch.relu, 'aten::relu'], # TODO support method relu
+        # [torch.sigmoid, 'aten::sigmoid'],
+        # [torch.tanh, 'aten::tanh'],
+        # [nn.LeakyReLU(0.1, inplace=False), 'aten::leaky_relu'],
+        # [nn.Hardtanh(inplace=False), 'aten::hardtanh'],
+        # [nn.GELU(approximate="none"), 'aten::gelu'],
+        # [nn.GELU(approximate="tanh"), 'aten::gelu'],
+    ]
+    return eltwise_list
+
+def test_conv_relu(eltwise_fn, bias, input_shape):
+    class M(nn.Module):
+        def __init__(self, eltwise_fn, in_channels, out_channels, bias, **kwargs):
+            super(M, self).__init__()
+            self.linear = torch.nn.Linear(in_channels, out_channels, bias=bias, **kwargs)
+            self.eltwise = eltwise_fn
+
+        def forward(self, x):
+            x = self.linear(x)
+            x = self.eltwise(x)
+            return x    
+
+    mod = M(eltwise_fn, input_shape[-1], 10, bias).eval()          
 
     @torchdynamo.optimize("inductor")
     def fn(x):
@@ -28,13 +50,13 @@ def test_conv_relu(bias, input_shape):
     fn(v)
     fn(v)
 
-
-for input_shape in [
-    [2, 3, 10],
-    [2, 10]]:
-    for bias in [
-        True,
-        False]:
-        test_conv_relu(bias, input_shape)
+for eltwise_fn in _eltwise_list():
+    for input_shape in [
+        [2, 3, 10],
+        [2, 10]]:
+        for bias in [
+            True,
+            False]:
+            test_conv_relu(eltwise_fn[0], bias, input_shape)
 
 print("done")
