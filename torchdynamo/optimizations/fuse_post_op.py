06b4fc53d73a95c38a4993c0e7d4759ac826aaac
chunyuan <chunyuan.wu@intel.com>
2022-09-25 14:40:33 +0000

    use a single module for one operand eltwise replacement

--- torchdynamo/optimizations/fuse_post_op.py
+++ torchdynamo/optimizations/fuse_post_op.py
@@ -5,42 +5,25 @@ import torch.fx as fx
 from torch.fx.experimental.optimization import matches_module_pattern, replace_node_module
 
 
-class LinearReLU(torch.nn.Linear):
+class LinearEltwiseOneOperand(torch.nn.Linear):
     def __init__(self, in_features, out_features, bias=True, device=None, dtype=None):
-        super(LinearReLU, self).__init__(in_features, out_features, bias=bias,
+        super(LinearEltwiseOneOperand, self).__init__(in_features, out_features, bias=bias,
             device=device, dtype=dtype)
     
     def forward(self, input):
-        y = torch.ops.mkldnn_prepacked.linear_relu(input, self.weight, self.bias, "relu")
+        y = torch.ops.mkldnn_prepacked.linear_relu(input, self.weight, self.bias, self.attr)
         return y
 
-# TODO: dupllicate with LinearReLU
-class LinearSigmoid(torch.nn.Linear):
-    def __init__(self, in_features, out_features, bias=True, device=None, dtype=None):
-        super(LinearSigmoid, self).__init__(in_features, out_features, bias=bias,
-            device=device, dtype=dtype)
-    
-    def forward(self, input):
-        y = torch.ops.mkldnn_prepacked.linear_relu(input, self.weight, self.bias, "sigmoid")
-        return y
-
-def fuse_linear_relu_eval(linear, relu):
-    linear_relu = LinearReLU(linear.in_features,
+def fuse_linear_eltwise_eval(linear, attr):
+    linear_relu = LinearEltwiseOneOperand(linear.in_features,
                               linear.out_features,
                               linear.bias is not None,
                               linear.weight.device,
                               linear.weight.dtype)
     linear_relu.__dict__ = copy.deepcopy(linear.__dict__)
+    # TODO: set this in init func is not working, due to copy __dict__??
+    linear_relu.attr = attr
     return linear_relu
-
-def fuse_linear_sigmoid_eval(linear, sigmoid):
-    linear_sigmoid = LinearSigmoid(linear.in_features,
-                              linear.out_features,
-                              linear.bias is not None,
-                              linear.weight.device,
-                              linear.weight.dtype)
-    linear_sigmoid.__dict__ = copy.deepcopy(linear.__dict__)
-    return linear_sigmoid
 
 def fuse_post_op(gm, example_inputs):
     modules = dict(gm.named_modules())
@@ -50,12 +33,12 @@ def fuse_post_op(gm, example_inputs):
         (torch.nn.Linear, torch.nn.ReLU),
         (torch.nn.Linear, torch.nn.Sigmoid),
     ]
-    replacements = [
-        fuse_linear_relu_eval,
-        fuse_linear_sigmoid_eval,
+    attr_names = [
+        "relu",
+        "sigmoid",
     ]
-    assert len(patterns) == len(replacements), "pattern and replacement length should be equal"
-    for pattern, replace_func in zip(patterns, replacements):
+    assert len(patterns) == len(attr_names), "pattern and replacement length should be equal"
+    for pattern, attr_name in zip(patterns, attr_names):
         for node in new_graph.nodes:
             if matches_module_pattern(pattern, node, modules):
                 if len(node.args[0].users) > 1:  # Output of bn is used by other nodes
@@ -69,7 +52,7 @@ def fuse_post_op(gm, example_inputs):
                     tensors.append(linear.bias)
                 is_cpu = all(x.device == torch.device('cpu') for x in tensors)
                 if eval_mode and is_cpu:
-                    fused_linear = replace_func(linear, eltwise)
+                    fused_linear = fuse_linear_eltwise_eval(linear, attr_name)
                     replace_node_module(node.args[0], modules, fused_linear)
                     node.replace_all_uses_with(node.args[0])
                     new_graph.erase_node(node)
