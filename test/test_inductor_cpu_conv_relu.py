7323eaeb09c500bd1322af9a65e9cfd770e64505
chunyuan <chunyuan.wu@intel.com>
2022-09-26 13:17:00 +0000

    ut: remove clamp related code

--- test/test_inductor_cpu_conv_relu.py
+++ test/test_inductor_cpu_conv_relu.py
@@ -5,59 +5,6 @@ from torchdynamo.testing import same
 
 torchdynamo.config.raise_on_backend_error = False
 
-def _clamp_modules():
-    class MNoOpt(nn.Module):
-        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
-            super(MNoOpt, self).__init__()
-            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
-
-        def forward(self, x):
-            x = self.conv(x)
-            x = torch.clamp(x, min=-0.5, max=0.9)
-            return x
-
-    class MInf(nn.Module):
-        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
-            super(MInf, self).__init__()
-            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
-
-        def forward(self, x):
-            x = self.conv(x)
-            x = torch.clamp(x, min=0, max=float('inf'))
-            return x
-
-    class MNegInf(nn.Module):
-        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
-            super(MNegInf, self).__init__()
-            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
-
-        def forward(self, x):
-            x = self.conv(x)
-            x = torch.clamp(x, min=float('-inf'), max=0)
-            return x
-
-    class MOptMin(nn.Module):
-        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
-            super(MOptMin, self).__init__()
-            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
-
-        def forward(self, x):
-            x = self.conv(x)
-            x = torch.clamp(x, max=2)
-            return x
-
-    class MOptMax(nn.Module):
-        def __init__(self, m, in_channels, out_channels, bias, **kwargs):
-            super(MOptMax, self).__init__()
-            self.conv = m(in_channels, out_channels, bias=bias, **kwargs)
-
-        def forward(self, x):
-            x = self.conv(x)
-            x = torch.clamp(x, min=0)
-            return x
-
-    return [MNoOpt, MInf, MNegInf, MOptMin, MOptMax]
-
 def _eltwise_list():
     eltwise_list = [
         [nn.ReLU(), 'aten::relu'],
@@ -68,11 +15,6 @@ def _eltwise_list():
         [nn.Hardtanh(min_val=-0.5, max_val=4, inplace=False), 'aten::hardtanh'],
         [nn.GELU(approximate="none"), 'aten::gelu'],
         [nn.GELU(approximate="tanh"), 'aten::gelu'],
-
-        # TODO: support inplace
-        # [torch.relu, 'aten::relu'], # TODO support method relu
-        # [torch.sigmoid, 'aten::sigmoid'],
-        # [torch.tanh, 'aten::tanh'],
     ]
     return eltwise_list
 
@@ -96,17 +38,8 @@ def test_conv_relu(eltwise_fn, bias, input_shape):
 
     v = torch.randn(input_shape)
 
-    # v = v.to(torch.bfloat16)
-    # mod = mod.to(torch.bfloat16)
-
-    print("#" * 50)
     result = fn(v)
-    print("#" * 50)
     assert same(result, mod(v))
-
-    print("run")
-    fn(v)
-    fn(v)
 
 for eltwise_fn in _eltwise_list():
     for input_shape in [
@@ -116,37 +49,3 @@ for eltwise_fn in _eltwise_list():
             True,
             False]:
             test_conv_relu(eltwise_fn[0], bias, input_shape)
-
-# # clamp:
-# modules = _clamp_modules()
-# for M in modules:
-#     for input_shape in [
-#         [2, 3, 10],
-#         [2, 10]]:
-#         for bias in [
-#             True,
-#             False]:
-#             mod = M(nn.Linear, input_shape[-1], 10, bias)
-
-#             mod.eval()
-
-#             # TODO: fix duplicate
-#             @torchdynamo.optimize("inductor")
-#             def fn(x):
-#                 return mod(x)
-
-#             v = torch.randn(input_shape)
-
-#             # v = v.to(torch.bfloat16)
-#             # mod = mod.to(torch.bfloat16)
-
-#             print("#" * 50)
-#             result = fn(v)
-#             print("#" * 50)
-#             assert same(result, mod(v))
-
-#             print("run")
-#             fn(v)
-#             fn(v)            
-
-print("done")
