9a2f2dbe384725df0375560b3c4a7f0728008360
chunyuan <chunyuan.wu@intel.com>
2022-10-10 14:12:36 +0000

    add linear-binary ut # TODO: remove duplicated code in UT, test more cases in UT

--- test/test_linear_fusion_cpu.py
+++ test/test_linear_fusion_cpu.py
@@ -1,3 +1,5 @@
+import itertools
+
 import torch
 import torch.nn as nn
 from torch.testing._internal.common_quantization import NodeSpec as ns
@@ -6,8 +8,9 @@ from torch.testing._internal.common_utils import run_tests
 
 import torchdynamo
 from torchdynamo.testing import same
-from torchinductor.overrides import LinearEltwise
+from torchinductor.overrides import LinearEltwise, LinearBinary
 from torchinductor.overrides import fuse_fx
+from torch.fx.passes.shape_prop import ShapeProp
 
 torchdynamo.config.raise_on_backend_error = False
 
@@ -64,6 +67,47 @@ class TestFuseFx(QuantizationTestCase):
                 for bias in [True, False]:
                     self._test_linear_eltwise(eltwise_fn, bias, input_shape)
 
+
+    def test_linear_binary(self):
+        class M(nn.Module):
+            def __init__(self, eltwise_fn, in_channels, out_channels, bias, **kwargs):
+                super(M, self).__init__()
+                self.linear = torch.nn.Linear(
+                    in_channels, out_channels, bias=bias, **kwargs
+                )
+                self.eltwise = eltwise_fn
+
+            def forward(self, x, y):
+                x = self.linear(x)
+                x = self.eltwise(x, y)
+                # TODO: test linear on both left and right side
+                # x = self.eltwise(y, x)
+                return x
+
+        out_feature = 20        
+        for binary_ops in [torch.add, torch.sub]:
+            options = itertools.product([[2, 3, 10], [2, 10]], [True, False])
+            for input_shape, bias in options:
+                mod = M(binary_ops, input_shape[-1], out_feature, bias).eval()
+
+                @torchdynamo.optimize("inductor")
+                def fn(x, y):
+                    return mod(x, y)
+
+                v = torch.randn(input_shape)
+
+                other = torch.randn(input_shape[:-1] + [out_feature])
+
+                example_inputs = [v, other]
+                gm = torch.fx.symbolic_trace(mod)
+                ShapeProp(gm).propagate(*example_inputs)
+
+                fused_gm = fuse_fx(gm, example_inputs)
+                expected_nodes = [ns.call_module(LinearBinary)]
+                self.checkGraphModuleNodes(fused_gm, expected_node_list=expected_nodes)
+
+                result = fn(*example_inputs)
+                assert same(result, mod(*example_inputs))
 
 if __name__ == "__main__":
     run_tests()
