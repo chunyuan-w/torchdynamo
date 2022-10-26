79aa256c3d8d3ee3b026aa9aac89b4fe0eeaad3e
chunyuan <chunyuan.wu@intel.com>
2022-10-26 15:37:15 +0000

    refactor the ut

--- test/test_softmax_torchinductor.py
+++ test/test_softmax_torchinductor.py
@@ -1,67 +1,54 @@
 import math
 import torch
 import torch.nn as nn
-import numpy as np
 
+from torch.testing._internal.common_utils import TestCase, run_tests
 # import torch._dynamo
 import torchdynamo
 from torch._inductor import config
-from torchdynamo.testing import same
 
 config.cpp.simdlen = 8
 config.dynamic_shapes = True
 
-
-@torchdynamo.optimize()
-def test_softmax(a):
-	return torch.nn.functional.softmax(a)
-	# return torch.matmul(a, torch.transpose(a, 0, 1))
-
-
-# is_contiguous = True
-# shape = (2, 512)
-# a = torch.rand(shape)
-# n_iter = 5
-
-# for _ in range(n_iter):
-# 	test_softmax(a)
+class TestCppWrapper(TestCase):
+	def _common(self, m, inputs):
+		with torch.no_grad():
+			optimized_mod = torchdynamo.optimize()(m)
+			dynamo_result = optimized_mod(*inputs)
+			ref_result = m(*inputs)
+			self.assertEqual(dynamo_result, ref_result)
 
 
-# dynamo_result = test_softmax(a)
-# ref_result = torch.nn.functional.softmax(a)
-# print(dynamo_result)
-# print(ref_result)
+	def test_single_kernel(self):
+		def func_softmax(a):
+			return torch.nn.functional.softmax(a)
 
-# np.testing.assert_allclose(dynamo_result.numpy(), ref_result.numpy(), atol=1e-6)
+		shape = (2, 512)
+		a = torch.rand(shape)
 
-class MHAScoresCalculation(nn.Module):
-	def __init__(self, dim_per_head, softmax_dim=-1):
-			super(MHAScoresCalculation, self).__init__()
-			self.softmax = nn.Softmax(dim=softmax_dim)
-			self.dim_per_head = dim_per_head
-	
-	def forward(self, mat1, mat2, bias):
-		mat1 = mat1 / math.sqrt(self.dim_per_head)
-		qk = torch.matmul(mat1, mat2.transpose(2, 3))
-		scores = qk + bias
-		return self.softmax(scores), scores
-		# return self.softmax(scores)
+		self._common(func_softmax, [a])
 
+	def test_two_kernels(self):
+		class MHAScoresCalculation(nn.Module):
+			def __init__(self, dim_per_head, softmax_dim=-1):
+					super(MHAScoresCalculation, self).__init__()
+					self.softmax = nn.Softmax(dim=softmax_dim)
+					self.dim_per_head = dim_per_head
+			
+			def forward(self, mat1, mat2, bias):
+				mat1 = mat1 / math.sqrt(self.dim_per_head)
+				qk = torch.matmul(mat1, mat2.transpose(2, 3))
+				scores = qk + bias
+				return self.softmax(scores), scores
 
+		seq_length = 384
+		mat1 = torch.randn(56, 16, seq_length, 64)
+		mat2 = torch.randn(56, 16, seq_length, 64)
+		bias = torch.randn(56, 16, seq_length, seq_length)
 
-seq_length = 384
-mat1 = torch.randn(56, 16, seq_length, 64)
-mat2 = torch.randn(56, 16, seq_length, 64)
-bias = torch.randn(56, 16, seq_length, seq_length)
-with torch.no_grad():
-	mha = MHAScoresCalculation(64)
-	optimized_mod = torchdynamo.optimize()(mha)
-	dynamo_result = optimized_mod(mat1, mat2, bias)
-	ref_result = mha(mat1, mat2, bias)
+		mha = MHAScoresCalculation(64)
 
-	assert same(dynamo_result, ref_result)
-	# np.testing.assert_allclose(dynamo_result.numpy(), ref_result.numpy(), atol=1e-6)
+		self._common(mha, [mat1, mat2, bias])			
 
-
-
-print("#" * 50)
+if __name__ == "__main__":
+    run_tests()
